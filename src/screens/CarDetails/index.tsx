302c977976df2a615455972f74afdd716c5fa5a1
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-03-17 15:21:38 -0300

    Componente de Slider

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -3,16 +3,23 @@ import React from "react";
 import { StatusBar } from "react-native";
 
 import { BackButton } from "../../components/BackButton";
+import { ImageSlider } from "../../components/ImageSlider";
 
-import { Container, Header } from "./styles";
+import { Container, Header, CarImages } from "./styles";
 
 export function CarDetails() {
   return (
     <Container>
-      <StatusBar />
       <Header>
         <BackButton onPress={() => {}} />
       </Header>
+      <CarImages>
+        <ImageSlider
+          imagesUrl={[
+            "https://cdn.sitewebmotors.com.br/uploads/userGallery/5fcfe53240728.png",
+          ]}
+        />
+      </CarImages>
     </Container>
   );
 }
