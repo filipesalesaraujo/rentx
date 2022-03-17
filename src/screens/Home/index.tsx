93f5436ce62d134455a586b7ba156e10066a5af6
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-03-17 14:42:23 -0300

    Criação do Card Car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,10 +5,34 @@ import { StatusBar } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 
 import Logo from "../../assets/logo.svg";
+import { Car } from "../../components/Car";
 
 import { Container, Header, HeaderContent, TotalCars } from "./styles";
 
 export function Home() {
+
+  const carDataOne = {
+    brand: "Audi",
+    name: "RS 5 Coupé",
+    rent: {
+      period: "ao dia",
+      price: 120,
+    },
+    thumbnail:
+      "https://cdn.sitewebmotors.com.br/uploads/userGallery/5fcfe53240728.png",
+  };
+
+  const carDataTwo = {
+    brand: "Porsche",
+    name: "Panamera",
+    rent: {
+      period: "ao dia",
+      price: 340,
+    },
+    thumbnail:
+      "https://www.mazettoseguros.com.br/blog/wp-content/uploads/2019/10/seguro-panamera-corretora-de-seguros-700x350.png",
+  };
+
   return (
     <Container>
       <StatusBar
@@ -22,6 +46,8 @@ export function Home() {
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
+      <Car data={carDataOne} />
+      <Car data={carDataTwo} />
     </Container>
   );
 }
