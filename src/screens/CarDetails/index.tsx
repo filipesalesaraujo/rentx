efc358be606e79545fe1508228b44098b0939b7d
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-03-17 15:33:50 -0300

    Continuando a interface de Detalhes

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -5,7 +5,20 @@ import { StatusBar } from "react-native";
 import { BackButton } from "../../components/BackButton";
 import { ImageSlider } from "../../components/ImageSlider";
 
-import { Container, Header, CarImages } from "./styles";
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+} from "./styles";
 
 export function CarDetails() {
   return (
@@ -20,6 +33,24 @@ export function CarDetails() {
           ]}
         />
       </CarImages>
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+        <About>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor
+          at risus sed accumsan. Aliquam scelerisque risus in nisi aliquet
+          ultrices. Nulla dignissim rhoncus porttitor. Nulla in pharetra enim.
+          Nulla a volutpat massa.
+        </About>
+      </Content>
     </Container>
   );
 }
