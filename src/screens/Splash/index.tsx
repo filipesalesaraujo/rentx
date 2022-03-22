c46e4e8b57ada35aea7f5046bf5436f48c124fa1
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-03-22 17:22:16 -0300

    Transições

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,22 +1,33 @@
 import React from "react";
-import { Button, StyleSheet } from "react-native";
+import { Button, StyleSheet, Dimensions } from "react-native";
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
+  withTiming,
+  Easing,
 } from "react-native-reanimated";
 
 import { Container } from "./styles";
+
+const WIDTH = Dimensions.get("window").width;
 
 export function Splash() {
   const animation = useSharedValue(0);
   const animatedStyles = useAnimatedStyle(() => {
     return {
-      transform: [{ translateX: animation.value }],
+      transform: [
+        {
+          translateX: withTiming(animation.value, {
+            duration: 500,
+            easing: Easing.bezier(0,1.9,1,-1.42),
+          }),
+        },
+      ],
     };
   });
 
-  function handleAnimationPosition(){
-      animation.value = Math.random() * 100;
+  function handleAnimationPosition() {
+    animation.value = Math.random() * (WIDTH - 100);
   }
 
   return (
