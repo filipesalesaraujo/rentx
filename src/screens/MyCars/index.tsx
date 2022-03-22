192c87238d789829dabec8cf55770ab5b4f9fbf4
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-03-22 09:27:42 -0300

    Listando Agendamentos do Usuário

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import {
-  Container
-} from './styles';
+import { api } from "../../services/api";
+import { CarDTO } from "../../dtos/CarDTO";
 
-export function MyCars(){
-  return (
-    <Container>
+import { Container } from "./styles";
 
-    </Container>
-  );
+export function MyCars() {
+  const [cars, setCars] = useState<CarDTO[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchCars() {
+      try {
+        const response = await api.get(`/schedules_byuser?user_id=1`);
+        setCars(response.data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchCars();
+  }, []);
+  return <Container></Container>;
 }
